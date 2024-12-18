ea94eb371621fe67253124eb5e15437566aefb5c
abg74123 <bisza_i.d@hotmail.com>
2024-12-18 19:00:57 +0700

    add:enum role

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { add } from './utils.js';
 export * as AiFlowCrmSubscription from './type-subscription.js';
 export * as AiFlowCrmBroadcast from './type-broadcast.js';
+export * as AiFlowCrmRoleEnum from './role-enum.js';
---
+++ src/role-enum.ts
@@ -0,0 +1,61 @@
+export enum USER_ROLE {
+    'OWNER' = 'owner',
+    'SUPER_ADMIN' = 'super-admin',
+    'ADMIN' = 'admin',
+    'STAFF' = 'staff',
+    'AGENT' = 'agent',
+}
+
+export enum TemplateReplyPermission {
+    'MANAGE' = 'template-reply-manage',
+}
+
+export enum ContactPermission {
+    'MANAGE' = 'contact-manage',
+}
+
+export enum ChatBotPermission {
+    'MANAGE' = 'chatbot-manage',
+}
+
+export enum ChatPermission {
+    'MANAGE' = 'chat-manage',
+    'MANAGE_ME' = 'chat-manage-me',
+    'MANAGE_TEAM' = 'chat-manage-team',
+    'MANAGE_NOT_ASSIGNED' = 'chat-manage-not-assigned',
+    'MANAGE_ALL' = 'chat-manage-all',
+    'MANAGE_SPAM' = 'chat-manage-spam',
+}
+
+export enum BroadcastPermission {
+    'MANAGE' = 'broadcast-manage',
+}
+
+export enum ManagementPermission {
+    'MANAGE' = 'management-manage',
+}
+
+export enum SystemPermission {
+    'MANAGE' = 'system-manage',
+}
+
+export enum FilePermission {
+    'MANAGE' = 'file-manage',
+}
+
+export enum IAMPermission {
+    'MANAGE' = 'iam-manage',
+    'CREATE_STAFF' = 'iam-create-staff',
+    'ACCESS_COMPANY' = 'iam-access-company',
+}
+
+export type permissions =
+    | ChatPermission
+    | BroadcastPermission
+    | ManagementPermission
+    | SystemPermission
+    | IAMPermission
+    | ChatBotPermission
+    | ContactPermission
+    | FilePermission
+    | TemplateReplyPermission;
