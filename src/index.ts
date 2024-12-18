1ccfe1a7b6d47d2c6d7d3a35fd45c9025b2ee879
abg74123 <bisza_i.d@hotmail.com>
2024-12-18 19:06:14 +0700

    add:enum role

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { add } from './utils.js';
 export * as AiFlowCrmSubscription from './type-subscription.js';
 export * as AiFlowCrmBroadcast from './type-broadcast.js';
-export * as AiFlowCrmRoleEnum from './role-enum.js';
+export * as AiFlowCrmSubRole from './type-sub-role.js';
